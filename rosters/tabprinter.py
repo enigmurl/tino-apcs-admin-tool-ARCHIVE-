fa241731c96db8db9c5567c4d501c96a6b0fbb63
Manu Bhat <manu.s.bhat@gmail.com>
2022-05-21 14:37:10 -0700

    Modify git ignore

--- rosters/tabprinter.py
+++
@@ -1,9 +0,0 @@
-with open("Period_1_APCS_Ferrante.txt","r") as f:
-    data = f.read();
-
-
-data = data.replace("          "," \t ")
-
-
-with open("Period_1_APCS_Ferrante_new.txt","w") as f:
-    f.write(data);
